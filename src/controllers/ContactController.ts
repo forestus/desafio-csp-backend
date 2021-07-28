b5dd86e6f4265e10e7530db00f969646547b34f5
forestus <guilhermepm164@gmail.com>
2021-07-28 03:05:25 -0300

    chore: add ContactController findAll method

--- src/controllers/ContactController.ts
+++ src/controllers/ContactController.ts
@@ -3,7 +3,7 @@ import { AppError } from '@errors/AppError'
 import { ContactsServices } from '@services/Posts/ContactsServices'
 
 class ContactController {
-  // Cria uma nova Postagem recebendo os dados pelo corpo da requisição, retorna os dados da Postagem criada e seu relacionamento com status correspondente.
+  // Cria um novo Contato recebendo os dados pelo corpo da requisição, retorna os dados do Contato criado e seu relacionamento com status correspondente.
   async store (request: Request, response: Response): Promise<Response> {
     const { firstname, lastname, email, phones } = request.body
     const contactsService = new ContactsServices()
@@ -13,6 +13,17 @@ class ContactController {
       return response.status(201).json({
         ...contact
       })
+    } catch (error) {
+      throw new AppError(error)
+    }
+  }
+
+  // Busca todos os Contatos armazenados no Banco, retorna os dados dos contatos criados e seus relacionamentos em uma Array com status correspondente.
+  async findAll (request: Request, response: Response): Promise<Response> {
+    const contactsService = new ContactsServices()
+    try {
+      const contacts = await contactsService.findAll()
+      return response.status(200).json(contacts)
     } catch (error) {
       throw new AppError(error)
     }
